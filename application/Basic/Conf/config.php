437aeae20d9a9c49c00c5215037ca1bbcf5acafe
Boxjan <boxjan@outlook.com>
2017-11-28 15:33:07 +0800

    add DB config and add sql connet config

--- application/Basic/Conf/config.php
+++ application/Basic/Conf/config.php
@@ -1,4 +1,15 @@
 <?php
 return array(
+	'DB_TYPE' => 'mysql',
+	'DB_HOST' => '127.0.0.1',
+	'DB_NAME' => 'jol',
+	'DB_USER' => 'root',
+	'DB_PWD' => 'root',
+
+	'EACH_PAGE' => 20,
+	'PAGE_NUM' => 10,
+	
+	'ISDEBUG' => true,
+	
 	//'配置项'=>'配置值'
 );
---
+++ application/Basic/Constant/DbConfig/ContestProblemListConfig.class.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 
+ * Created by dream.
+ * User: Boxjan
+ * Datetime: 11/28/2017 15:29
+ */
+
+ namespace Constant\DbConfig;
+ 
+ class ContestProblemListConfig 
+ {
+     const TABLE_NAME = "contest_problem";
+
+     public static $TABLE_FILED = array(
+         'problem_id' => 'int',
+         'contest_id' => 'int',
+         'title' => 'char',
+         'num' => 'int',
+     );
+ }
