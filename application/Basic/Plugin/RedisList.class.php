83d0a1871b29988d24164ef6b6334cf6d2dfb758
boxjan <boxjan@outlook.com>
2018-05-29 20:46:47 +0800

    我也不知道我写了什么，自己看吧

--- application/Basic/Plugin/RedisList.class.php
+++ application/Basic/Plugin/RedisList.class.php
@@ -8,54 +8,27 @@
 
 namespace Basic\Plugin;
 
+use Think\Cache\Driver\Redis;
 /**
  * Redis缓存驱动
  * 要求安装phpredis扩展：https://github.com/phpredis/phpredis
  */
 
-class RedisList
+class RedisList extends Redis
 {
-    protected $listName;
+    private static $_instance = null;
 
-    protected $handle;
-
-    public function __construct($listName){
-        if ( !extension_loaded('redis') ) {
-            E(L('_NOT_SUPPORT_').':redis');
-        }
-
-        $option = array (
-            'host'          => C('REDIS_HOST') ? : '127.0.0.1',
-            'port'          => C('REDIS_PORT') ? : 6379,
-            'timeout'       => C('DATA_CACHE_TIMEOUT') ? : false,
-        );
-        $this->listName = $listName;
-
-        $this->handle = new \Redis();
-        $status = true;
-        try {
-            $status = $this->handle->pconnect($option['host'], $option['port'], $option['timeout']);
-        } catch(\Exception $e) {
-            E("Redis Connect Fail!");
-        }
-        if ($status === false) E("Redis Connect Fail!");
+    private function __construct() {
     }
 
-    public function push($data) {
-        $status = $this->handle->lPush($this->listName, $data);
-        if ($status === false) {
-            \Think\Log::record("Redis list push error!");
-            Log::error("Redis list push error!");
-        }
+    private function __clone() {
     }
 
-    public function pop() {
-        $res = $this->handle->rPop($this->listName);
-        if($$res === false) {
-            \Think\Log::record("Redis list pop error!");
-            Log::error("Redis list pop error!");
+    public static function instance() {
+        if (is_null(self::$_instance)) {
+            self::$_instance = new self;
         }
-        return $res;
+        return self::$_instance;
     }
-
+    
 }
---
+++ application/Common/Conf/redis.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Redis信息配置
+ *
+ * Created by Magic.
+ * User: Boxjan
+ * Datetime: 05/29/2018 19:58
+ */
+
+ return array(
+    'REDIS_HOST' => '127.0.0.1',
+
+    'REDIS_PORT' => 6379,
+
+    'DATA_CACHE_TIMEOUT' => false,
+    
+ );
