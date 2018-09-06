2e63343410a355a8d1273a559334d2b02e4bfbfd
boxjan <boxjan@outlook.com>
2018-09-06 14:16:19 +0800

    Add Contest Module

---
+++ application/Contest/Controller/IndexController.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * drunk , fix later
+ * Created by Magic.
+ * User: jiaying
+ * Datetime: 01/12/2017 21:27
+ */
+
+namespace Exam\Controller;
+
+
+class IndexController extends TemplateController
+{
+    public function index() {
+        echo __CLASS__;
+    }
+}
